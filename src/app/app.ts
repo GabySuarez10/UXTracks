02c662a52f4133aed7a4b9feed9b2d6946b18ec2
GabySuarez10 <gabysuarezcardenas11@gmail.com>
2025-09-21 17:02:50 -0500

    prueba1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Prueba } from './prueba/prueba';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Prueba, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/routes.ts
@@ -0,0 +1,10 @@
+import {Routes} from '@angular/router';
+import { Prueba } from './prueba/prueba';
+const routeConfig: Routes = [ 
+      {
+      path: '',
+      component: Prueba,
+      title: 'prueba',
+    },
+  ];
+  export default routeConfig;
